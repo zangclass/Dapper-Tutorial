54c542c1b5b233664ecd3cec039d6ef188782b9f
ZangClass <ZangClass@ZANGCLASS>
2021-07-05 11:59:04 +0430

    آموزش قسمت 7 QuerySingleOrDefault و QuerySingle در Dapper

---
+++ 07-QuerySingle and QuerySingleOrDefault/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace _07_QuerySingle_and_QuerySingleOrDefault
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnQuerySingleOrDefault = new System.Windows.Forms.Button();
+            this.btnQuerySingle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnQuerySingleOrDefault
+            // 
+            this.btnQuerySingleOrDefault.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuerySingleOrDefault.Location = new System.Drawing.Point(12, 100);
+            this.btnQuerySingleOrDefault.Name = "btnQuerySingleOrDefault";
+            this.btnQuerySingleOrDefault.Size = new System.Drawing.Size(380, 82);
+            this.btnQuerySingleOrDefault.TabIndex = 3;
+            this.btnQuerySingleOrDefault.Text = "QuerySingleOrDefault";
+            this.btnQuerySingleOrDefault.UseVisualStyleBackColor = true;
+            this.btnQuerySingleOrDefault.Click += new System.EventHandler(this.btnQuerySingleOrDefault_Click);
+            // 
+            // btnQuerySingle
+            // 
+            this.btnQuerySingle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuerySingle.Location = new System.Drawing.Point(12, 12);
+            this.btnQuerySingle.Name = "btnQuerySingle";
+            this.btnQuerySingle.Size = new System.Drawing.Size(380, 82);
+            this.btnQuerySingle.TabIndex = 2;
+            this.btnQuerySingle.Text = "QuerySingle";
+            this.btnQuerySingle.UseVisualStyleBackColor = true;
+            this.btnQuerySingle.Click += new System.EventHandler(this.btnQuerySingle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 196);
+            this.Controls.Add(this.btnQuerySingleOrDefault);
+            this.Controls.Add(this.btnQuerySingle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnQuerySingleOrDefault;
+        private System.Windows.Forms.Button btnQuerySingle;
+    }
+}
+
---
+++ 07-QuerySingle and QuerySingleOrDefault/Form1.cs
@@ -0,0 +1,62 @@
+﻿using Dapper;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _07_QuerySingle_and_QuerySingleOrDefault
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string _connectionString = "Server=.; Database=DapperDB; Trusted_Connection=True;";
+        private void GetQuerySingle()
+        {
+            try
+            {
+                using (IDbConnection con = new SqlConnection(_connectionString))
+                {
+                    var sql = "select * from dbo.tblUser where id = @id";
+                    var result = con.QuerySingle<UserEntity>(sql,new { id = 1});
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void btnQuerySingle_Click(object sender, EventArgs e)
+        {
+            GetQuerySingle();
+        }
+        private void GetQuerySingleOrDefault()
+        {
+            try
+            {
+                using (IDbConnection con = new SqlConnection(_connectionString))
+                {
+                    var sql = "select * from dbo.tblUser where id >= @id";
+                    var result = con.QuerySingleOrDefault<UserEntity>(sql, new { id = 7 });
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void btnQuerySingleOrDefault_Click(object sender, EventArgs e)
+        {
+            GetQuerySingleOrDefault();
+        }
+    }
+}
